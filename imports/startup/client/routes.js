131553276d515e69c2c31c60aa90735d4f01b42d
Michał Bartkowiak <bartmichu@gmail.com>
2017-08-21 21:06:37 +0200

    Renderowanie menu aplikacji przez router

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { FlowRouter } from "meteor/kadira:flow-router";
 import { BlazeLayout } from "meteor/kadira:blaze-layout";
+import "/imports/ui/components/application_menu_T.js";
 import "/imports/ui/layouts/body_T.js";
 import "/imports/ui/pages/login_T.js";
 import "/imports/ui/pages/placeholder_T.js";
@@ -18,7 +19,7 @@ FlowRouter.route("/", {
   name: "index",
   action() {
     BlazeLayout.render("body_T", {
-      app_menu_section: "",
+      app_menu_section: "application_menu_T",
       app_main_section: "placeholder_T",
     });
   },
