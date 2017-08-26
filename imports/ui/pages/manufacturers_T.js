b1d1fa4972026926f715357803d1d342efaed802
Michał Bartkowiak <bartmichu@gmail.com>
2017-08-26 20:29:52 +0200

    Dodanie templatki listy producentów

---
+++ imports/ui/pages/manufacturers_T.js
@@ -0,0 +1,37 @@
+import { Template } from "meteor/templating";
+import { FlowRouter } from "meteor/kadira:flow-router";
+import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
+import "/imports/ui/components/list_placeholder_T.js";
+import "/imports/ui/components/loading_T.js";
+import "./manufacturers_T.html";
+
+
+Template.manufacturers_T.onCreated(() => {
+  Template.instance().subscribe("manufacturers.private");
+});
+
+
+Template.manufacturers_T.helpers({
+  getSubscriptionDataH() {
+    return Manufacturers.find();
+  },
+  numberOfElementsH() {
+    return Manufacturers.find().count();
+  },
+  isEmptyCollectionH() {
+    return Manufacturers.find().count() === 0;
+  },
+  getLabelH(pole) {
+    return Manufacturers.simpleSchema().label(pole);
+  },
+});
+
+
+Template.manufacturers_T.events({
+  submit(event) {
+    event.preventDefault();
+  },
+  "click #element": () => {
+    FlowRouter.go("manufacturers.producent", { _id: this._id });
+  },
+});
