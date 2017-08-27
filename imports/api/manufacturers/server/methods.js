edc32d3b58c2d694fd964db71ad93f2cf9750237
Michał Bartkowiak <bartmichu@gmail.com>
2017-08-27 19:53:38 +0200

    Zmiana polskich nazw zmiennych na angielskie

--- imports/api/manufacturers/server/methods.js
+++ imports/api/manufacturers/server/methods.js
@@ -2,46 +2,46 @@ import { Meteor } from "meteor/meteor";
 import { ValidatedMethod } from "meteor/mdg:validated-method";
 import { check } from "meteor/check";
 import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
-// import { Models } from "/imports/api/collections/urzadzenia/urzadzenia.js";
+// import Models from "/imports/api/models/models.js";
 import { manufacturerSchema } from "/imports/api/manufacturers/schema.js";
 
 
-export const wstawProducenta = new ValidatedMethod({
-  name: "producenci.wstaw",
-  validate(producent) {
-    const zalogowanyUzytkownikId = Meteor.userId();
-    if (zalogowanyUzytkownikId === null) {
+export const insertManufacturer = new ValidatedMethod({
+  name: "manufacturers.insert",
+  validate(manufacturer) {
+    const actualUserId = Meteor.userId();
+    if (actualUserId === null) {
       throw new Meteor.Error("Błąd wywołania metody");
     } else {
-      producent.uzytkownikId = zalogowanyUzytkownikId;
-      const kontekstWalidacji = manufacturerSchema.newContext();
-      if (kontekstWalidacji.validate(producent) !== true) {
+      manufacturer.uzytkownikId = actualUserId;
+      const validationContext = manufacturerSchema.newContext();
+      if (validationContext.validate(manufacturer) !== true) {
         throw new Meteor.Error("Błąd wywołania metody");
       }
     }
   },
-  run(producent) {
-    producent.uzytkownikId = Meteor.userId();
-    producent.nazwaSortowalna = producent.nazwa.toLowerCase();
-    producent.dataUtworzenia = new Date();
-    producent.dataModyfikacji = producent.dataUtworzenia;
-    return Manufacturers.insert(producent);
+  run(manufacturer) {
+    manufacturer.uzytkownikId = Meteor.userId();
+    manufacturer.nazwaSortowalna = manufacturer.nazwa.toLowerCase();
+    manufacturer.dataUtworzenia = new Date();
+    manufacturer.dataModyfikacji = manufacturer.dataUtworzenia;
+    return Manufacturers.insert(manufacturer);
   },
 });
 
 
-export const usunProducenta = new ValidatedMethod({
-  name: "producenci.usun",
+export const removeManufacturer = new ValidatedMethod({
+  name: "manufacturers.remove",
   validate(documentId) {
-    const zalogowanyUzytkownikId = Meteor.userId();
-    if (zalogowanyUzytkownikId === null) {
+    const actualUserId = Meteor.userId();
+    if (actualUserId === null) {
       throw new Meteor.Error("Błąd wywołania metody");
     } else {
       check(documentId, String);
-      const producent = Manufacturers.findOne({ _id: documentId });
-      // if (!producent || (producent.uzytkownikId !== zalogowanyUzytkownikId)) {
+      const manufacturer = Manufacturers.findOne({ _id: documentId });
+      // if (!manufacturer || (manufacturer.uzytkownikId !== actualUserId)) {
       //   throw new Meteor.Error("Błąd wywołania metody");
-      // } else if (Models.find({ producentId: producent._id }).count() > 0) {
+      // } else if (Models.find({ producentId: manufacturer._id }).count() > 0) {
       //   throw new Meteor.Error("Błąd wywołania metody");
       // }
     }
@@ -52,18 +52,18 @@ export const usunProducenta = new ValidatedMethod({
 });
 
 
-export const aktualizujProducenta = new ValidatedMethod({
-  name: "producenci.aktualizuj",
-  validate({ documentId, daneFormularza }) {
-    const zalogowanyUzytkownikId = Meteor.userId();
-    if (zalogowanyUzytkownikId === null) {
+export const updateManufacturer = new ValidatedMethod({
+  name: "manufacturers.update",
+  validate({ documentId, formData }) {
+    const actualUserId = Meteor.userId();
+    if (actualUserId === null) {
       throw new Meteor.Error("Błąd wywołania metody");
     } else {
       check(documentId, String);
-      const kontekstWalidacji = manufacturerSchema.newContext();
-      if (kontekstWalidacji.validate(daneFormularza) === true) {
-        const producent = Manufacturers.findOne({ _id: documentId });
-        if (!producent || (producent.uzytkownikId !== zalogowanyUzytkownikId)) {
+      const validationContext = manufacturerSchema.newContext();
+      if (validationContext.validate(formData) === true) {
+        const manufacturer = Manufacturers.findOne({ _id: documentId });
+        if (!manufacturer || (manufacturer.uzytkownikId !== actualUserId)) {
           throw new Meteor.Error("Błąd wywołania metody");
         }
       } else {
@@ -71,15 +71,15 @@ export const aktualizujProducenta = new ValidatedMethod({
       }
     }
   },
-  run({ documentId, daneFormularza }) {
+  run({ documentId, formData }) {
     return Manufacturers.update({ _id: documentId }, {
       $set: {
-        nazwa: daneFormularza.nazwa,
-        nazwaSortowalna: daneFormularza.nazwa.toLowerCase(),
-        "adres.ulica": daneFormularza.adres.ulica,
-        "adres.miejscowosc": daneFormularza.adres.miejscowosc,
-        "adres.kodPocztowy": daneFormularza.adres.kodPocztowy,
-        dodatkoweInformacje: daneFormularza.dodatkoweInformacje,
+        nazwa: formData.nazwa,
+        nazwaSortowalna: formData.nazwa.toLowerCase(),
+        "adres.ulica": formData.adres.ulica,
+        "adres.miejscowosc": formData.adres.miejscowosc,
+        "adres.kodPocztowy": formData.adres.kodPocztowy,
+        dodatkoweInformacje: formData.dodatkoweInformacje,
         dataModyfikacji: new Date(),
       },
     });
