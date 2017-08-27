8275e8476024c60f11cef0d9f286bc7c80498067
Michał Bartkowiak <bartmichu@gmail.com>
2017-08-27 09:29:40 +0200

    Zaimportowanie pliku z globalnymi funkcjami pomocniczymi podczas startu

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,4 +1,5 @@
 import "/imports/startup/client/accounts-configuration.js";
 import "/imports/startup/client/routes.js";
+import "/imports/startup/client/global-helpers.js";
 import "/imports/startup/client/tracker.js";
 import "/imports/startup/client/register-api.js";
