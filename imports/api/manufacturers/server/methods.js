c78ec287e49a7f669e88736052d2302eb1846627
Michał Bartkowiak <bartmichu@gmail.com>
2017-08-27 20:39:30 +0200

    Włączenie kodu sprawdzającego liczbę modeli

--- imports/api/manufacturers/server/methods.js
+++ imports/api/manufacturers/server/methods.js
@@ -2,7 +2,7 @@ import { Meteor } from "meteor/meteor";
 import { ValidatedMethod } from "meteor/mdg:validated-method";
 import { check } from "meteor/check";
 import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
-// import Models from "/imports/api/models/models.js";
+import Models from "/imports/api/models/models.js";
 import { manufacturerSchema } from "/imports/api/manufacturers/schema.js";
 
 
@@ -39,11 +39,11 @@ export const removeManufacturer = new ValidatedMethod({
     } else {
       check(documentId, String);
       const manufacturer = Manufacturers.findOne({ _id: documentId });
-      // if (!manufacturer || (manufacturer.uzytkownikId !== actualUserId)) {
-      //   throw new Meteor.Error("Błąd wywołania metody");
-      // } else if (Models.find({ producentId: manufacturer._id }).count() > 0) {
-      //   throw new Meteor.Error("Błąd wywołania metody");
-      // }
+      if (!manufacturer || (manufacturer.uzytkownikId !== actualUserId)) {
+        throw new Meteor.Error("Błąd wywołania metody");
+      } else if (Models.find({ producentId: manufacturer._id }).count() > 0) {
+        throw new Meteor.Error("Błąd wywołania metody");
+      }
     }
   },
   run(documentId) {
