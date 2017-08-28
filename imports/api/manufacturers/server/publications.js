25c06e16ee2c30fbdf8dce5d43401cb08da16577
Michał Bartkowiak <bartmichu@gmail.com>
2017-08-28 20:28:28 +0200

    Dodanie symulacji opóźnienia dla wersji rozwojowej

--- imports/api/manufacturers/server/publications.js
+++ imports/api/manufacturers/server/publications.js
@@ -3,6 +3,9 @@ import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
 
 
 Meteor.publish("manufacturers.private", () => {
+  // TODO usunąć w wersji produkcyjnej - symulacja opóźnienia połączenia
+  Meteor._sleepForMs(2000);
+
   const actualUserId = Meteor.userId();
   if (actualUserId) {
     const data = Manufacturers.find({ uzytkownikId: actualUserId });
