8688f44b9e8bd764b37e09cbecad891a30732201
Michał Bartkowiak <bartmichu@gmail.com>
2017-08-28 20:22:12 +0200

    Dodanie sprawdzania ID użytkownika przed publikacją

--- imports/api/manufacturers/server/publications.js
+++ imports/api/manufacturers/server/publications.js
@@ -3,10 +3,13 @@ import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
 
 
 Meteor.publish("manufacturers.private", () => {
-  const data = Manufacturers.find({ uzytkownikId: Meteor.userId() });
+  const actualUserId = Meteor.userId();
+  if (actualUserId) {
+    const data = Manufacturers.find({ uzytkownikId: actualUserId });
 
-  if (data) {
-    return data;
+    if (data) {
+      return data;
+    }
   }
 
   return this.ready();
