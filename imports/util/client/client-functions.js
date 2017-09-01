85694833ed55b0aed21c5252c621d169006d7adf
Michał Bartkowiak <bartmichu@gmail.com>
2017-09-01 19:50:41 +0200

    Dodanie kolejnych globalnych funkcji klienckich

--- imports/util/client/client-functions.js
+++ imports/util/client/client-functions.js
@@ -1,12 +1,35 @@
 import { Session } from "meteor/session";
+import { FlowRouter } from "meteor/kadira:flow-router";
+import { $ } from "meteor/jquery";
 
 
 const setDirty = state => Session.set("isDirty", state);
 
 
-const resetSessionVariables = () => {
-  setDirty(false);
+const setEditMode = (state) => {
+  Session.set("isDirty", false);
+  Session.set("isEditMode", state);
 };
 
 
-export { setDirty, resetSessionVariables };
+const getEditModeFromRoute = () => FlowRouter.current().route.name.split(".").reverse()[0] === "add";
+
+
+const resetSessionVariables = () => {
+  Session.set("isDirty", false);
+  Session.set("isEditMode", false);
+};
+
+
+const setFormLabels = (schema) => {
+  $("label").each(function setLabel() {
+    // TODO: sprawdzić arrow function (this?)
+    const label = $(this);
+    if (label.attr("id").split("-")[0] === "etykieta_pola") {
+      $(label).text(schema.label(label.attr("for")));
+    }
+  });
+};
+
+
+export { setDirty, resetSessionVariables, setEditMode, getEditModeFromRoute, setFormLabels };
