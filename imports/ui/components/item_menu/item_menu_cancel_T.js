cd2235830f99b00b5543b0a8e71610dc338e83ee
Michał Bartkowiak <bartmichu@gmail.com>
2017-09-02 21:04:59 +0200

    Dodanie templatki przycisku "anuluj"

---
+++ imports/ui/components/item_menu/item_menu_cancel_T.js
@@ -0,0 +1,42 @@
+import { Template } from "meteor/templating";
+import { FlowRouter } from "meteor/kadira:flow-router";
+import { getAddingModeFromRouter, setEditMode, routeBack } from "/imports/util/client/client-functions.js";
+import "./item_menu_cancel_T.html";
+
+
+Template.item_menu_cancel_T.onCreated(() => {
+  Template.instance().isAddingMode = getAddingModeFromRouter();
+});
+
+
+Template.item_menu_cancel_T.helpers({
+  buttonTextH() {
+    return Template.instance().isAddingMode ? "anuluj dodawanie" : "anuluj zmiany";
+  },
+});
+
+
+Template.item_menu_cancel_T.events({
+  "click #button-cancel": () => {
+    setEditMode(false);
+    if (Template.instance().isAddingMode) {
+      routeBack();
+    } else {
+      // TODO: wypelnianie danych formularza
+      const context = FlowRouter.current().route.group.name;
+      switch (context) {
+        case "offices": {
+          break;
+        }
+        case "manufacturers": {
+          break;
+        }
+        case "models": {
+          break;
+        }
+        default:
+          break;
+      }
+    }
+  },
+});
