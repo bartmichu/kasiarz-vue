c367a3ad8b0f690e95efd44e7d193e6c51ebeaca
Michał Bartkowiak <bartmichu@gmail.com>
2017-09-10 11:16:50 +0200

    Dodanie templatki okna modalnego akcji usuwania elementu

---
+++ imports/ui/components/application_menu/modal_delete_T.js
@@ -0,0 +1,29 @@
+import { Meteor } from "meteor/meteor";
+import { Template } from "meteor/templating";
+import { $ } from "meteor/jquery";
+import { FlowRouter } from "meteor/kadira:flow-router";
+import { routeBack } from "/imports/util/client/client-functions.js";
+import "./modal_delete_T.html";
+
+
+Template.modal_delete_T.rendered = () => {
+  $("#modal-delete").modal({
+    dismissible: false,
+    opacity: 0.8,
+  });
+};
+
+
+Template.modal_delete_T.events({
+  "click #button-delete-confirm": () => {
+    Meteor.call(FlowRouter.current().route.group.name.concat(".remove"), FlowRouter.getParam("_id"), function (error) {
+      if (error) {
+        // TODO: przetestować po zaimplementowaniu modal-delete-error
+        // $("#modal-delete-error").modal("open");
+      } else {
+        routeBack();
+        $("#modal-delete").modal("close");
+      }
+    });
+  },
+});
