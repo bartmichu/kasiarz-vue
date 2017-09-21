4c0e27c740cddf3ae3005b00f56e5cd988cfada9
Michał Bartkowiak <bartmichu@gmail.com>
2017-09-21 19:38:02 +0200

    Dodanie częściowo działającej templatki modelu urządzenia

---
+++ imports/ui/pages/models_item_T.js
@@ -0,0 +1,53 @@
+import { Session } from "meteor/session";
+import { Template } from "meteor/templating";
+import { Tracker } from "meteor/tracker";
+import { $ } from "meteor/jquery";
+import { getAddingModeFromRoute, setEditMode, setFormLabels } from "/imports/util/client/client-functions.js";
+import Models from "/imports/api/models/models.js";
+import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
+import "/imports/ui/components/item_menu/item_menu_cancel_T.js";
+import "/imports/ui/components/item_menu/item_menu_close_T.js";
+import "/imports/ui/components/item_menu/item_menu_delete_T.js";
+import "/imports/ui/components/item_menu/item_menu_edit_T.js";
+import "/imports/ui/components/item_menu/item_menu_save_T.js";
+import "./models_item_T.html";
+
+
+Template.models_item_T.onCreated(() => {
+  Template.instance().subscribe("manufacturers.private");
+  setEditMode(getAddingModeFromRoute());
+});
+
+
+Template.models_item_T.rendered = () => {
+  $("select").material_select();
+
+  setFormLabels(Models.simpleSchema());
+  // TODO: uzupełnianie danych formularza
+};
+
+
+Template.models_item_T.helpers({
+  manufacturersListH() {
+    return Manufacturers.find({}, { sort: { nazwa: 1 } });
+  },
+  fixDisabledSelectH() {
+    // FIXME: odwrotny stan disabled po przełączeniu isEditMode
+    $("select").material_select();
+  },
+});
+
+
+Template.models_item_T.events({
+  submit(event) {
+    event.preventDefault();
+  },
+  "input input, input textarea": () => {
+    if (Session.equals("isEditMode", true)) {
+      Session.set("isDirty", true);
+    }
+  },
+  "blur input, blur textarea": (event) => {
+    event.target.value = event.target.value.trim();
+  },
+});
