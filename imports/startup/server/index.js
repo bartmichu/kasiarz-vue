c0f7af36e187fe052f5498ec051fb119e62b5c46
Michał Bartkowiak <bartmichu@gmail.com>
2017-09-30 07:03:23 +0200

    Closes #1 - Instalacja i konfiguracja pakietu browser-policy

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,3 +1,4 @@
+import "/imports/startup/server/browserpolicy-configuration.js";
 import "/imports/startup/server/accounts-configuration.js";
 import "/imports/startup/server/accounts-fixtures.js";
 import "/imports/startup/server/register-api.js";
