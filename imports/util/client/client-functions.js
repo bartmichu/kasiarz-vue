5df80ea2212d14aabcf1485cb8e08eed2886624e
Michał Bartkowiak <bartmichu@gmail.com>
2017-10-11 20:27:23 +0200

    Dodanie wstępnej wersji funkcji automatycznie wypełniającej formularz

--- imports/util/client/client-functions.js
+++ imports/util/client/client-functions.js
@@ -1,6 +1,7 @@
 import { Session } from "meteor/session";
 import { FlowRouter } from "meteor/kadira:flow-router";
 import { $ } from "meteor/jquery";
+import { Materialize } from "meteor/materialize:materialize";
 import moment from "moment";
 
 
@@ -41,12 +42,48 @@ const setFormLabels = (schema) => {
 const formatDate = date => moment(date).format("DD-MM-YYYY, HH:mm");
 
 
+const escapeJq = id => `#${id.replace(/(:|\.|\[|\]|,|=|@)/g, "\\$1")}`;
+
+
+const setFormValues = (schema, data) => {
+  Object.keys(schema.getDefinition()).forEach((fieldLabel) => {
+    let valuesChanged = false;
+    let value = "";
+    let trueFieldLabel = fieldLabel;
+
+    if (trueFieldLabel.indexOf(".") !== -1) {
+      trueFieldLabel = trueFieldLabel.split(".")[1];
+      value = data[fieldLabel.split(".")[0]][trueFieldLabel];
+    } else {
+      value = data[trueFieldLabel];
+    }
+
+    const uiElement = $(escapeJq(fieldLabel));
+    if (uiElement.is("input,textarea")) {
+      if (value instanceof Date) {
+        uiElement.val(formatDate(value));
+        valuesChanged = true;
+      } else {
+        uiElement.val(value);
+        valuesChanged = true;
+      }
+    }
+
+    if (valuesChanged) {
+      Materialize.updateTextFields();
+    }
+  });
+};
+
+
 export {
   setDirty,
   formatDate,
+  escapeJq,
   resetSessionVariables,
   setEditMode,
   getAddingModeFromRoute,
   setFormLabels,
+  setFormValues,
   routeBack,
 };
