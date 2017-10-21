f11d38261de5c5b3ecfb9f22b2897700d3b0cc31
Michał Bartkowiak <bartmichu@gmail.com>
2017-10-21 22:13:02 +0200

    Odświeżenie danych formularza po anulowaniu zmian

--- imports/ui/components/item_menu/item_menu_cancel_T.js
+++ imports/ui/components/item_menu/item_menu_cancel_T.js
@@ -1,6 +1,8 @@
 import { Template } from "meteor/templating";
 import { FlowRouter } from "meteor/kadira:flow-router";
-import { getAddingModeFromRoute, setEditMode, routeBack } from "/imports/util/client/client-functions.js";
+import { getAddingModeFromRoute, setEditMode, routeBack, setFormValues } from "/imports/util/client/client-functions.js";
+import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
+import Models from "/imports/api/models/models.js";
 import "./item_menu_cancel_T.html";
 
 
@@ -22,21 +24,25 @@ Template.item_menu_cancel_T.events({
     if (Template.instance().isAddingMode) {
       routeBack();
     } else {
-      // TODO: wypelnianie danych formularza
       const context = FlowRouter.current().route.group.name;
+      let collection = null;
       switch (context) {
         case "offices": {
           break;
         }
         case "manufacturers": {
+          collection = Manufacturers;
           break;
         }
         case "models": {
+          collection = Models;
           break;
         }
         default:
           break;
       }
+
+      setFormValues(collection.simpleSchema(), collection.findOne({ _id: FlowRouter.getParam("_id") }));
     }
   },
 });
