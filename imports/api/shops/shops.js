95834fc9111727bed9e166be940a81c03661c617
Michał Bartkowiak <bartmichu@gmail.com>
2017-11-19 15:13:22 +0100

    Dodanie kolekcji z danymi serwisu

---
+++ imports/api/shops/shops.js
@@ -0,0 +1,29 @@
+import { Mongo } from "meteor/mongo";
+
+
+const Shops = new Mongo.Collection("Shops");
+Shops.allow({
+  insert() {
+    return false;
+  },
+  update() {
+    return false;
+  },
+  remove() {
+    return false;
+  },
+});
+Shops.deny({
+  insert() {
+    return true;
+  },
+  update() {
+    return true;
+  },
+  remove() {
+    return true;
+  },
+});
+
+
+export default Shops;
