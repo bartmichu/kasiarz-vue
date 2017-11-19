c8ffbdce85fd25b1e9324895653879ba12a6e4af
Michał Bartkowiak <bartmichu@gmail.com>
2017-11-19 15:17:12 +0100

    Usunięcie danych serwisu z profilu użytkownika

--- imports/startup/server/accounts-fixtures.js
+++ imports/startup/server/accounts-fixtures.js
@@ -12,36 +12,13 @@ Meteor.startup(() => {
           imie: "Imię",
           nazwisko: "Nazwisko",
         },
-        serwis: {
-          nazwa: "Projekt Kasiarz",
-          nip: "1234567890",
-          regonPesel: "987654321",
-          adres: {
-            kraj: "Polska",
-            wojewodztwo: "wielkopolskie",
-            gminaDzielnica: "Przykładowa",
-            ulica: "Zmyślona",
-            numerDomu: "99",
-            numerLokalu: "",
-            miejscowosc: "Przykładowa",
-            kodPocztowy: "09-876",
-            poczta: "Pocztowa",
-            skrytkaPocztowa: "",
-          },
-          telefon: "111222333",
-          email: "",
-          dodatkoweInformacje: "",
-          serwisanci: [{ imieNazwisko: "Serwisant Pierwszy", dodatkoweInformacje: "" }, { imieNazwisko: "Serwisant Drugi", dodatkoweInformacje: "" }],
-        },
       },
     });
 
     Accounts.createUser({
       username: "test",
       password: "test",
-      profile: {
-        serwis: {},
-      },
+      profile: {},
     });
   }
 });
