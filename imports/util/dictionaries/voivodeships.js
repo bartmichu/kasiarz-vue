5761f38da636aeb21c6d190d2bd097be1f89de42
Michał Bartkowiak <bartmichu@gmail.com>
2017-12-17 14:39:54 +0100

    Dodanie listy województw

---
+++ imports/util/dictionaries/voivodeships.js
@@ -0,0 +1,6 @@
+const voivodeships = ["dolnośląskie", "kujawsko-pomorskie",
+  "lubelskie", "lubuskie", "łódzkie", "małopolskie", "mazowieckie", "opolskie",
+  "podlaskie", "pomorskie", "śląskie", "świętokrzyskie", "warmińsko-mazurskie",
+  "wielkopolskie", "zachodniopomorskie"];
+
+export default voivodeships;
