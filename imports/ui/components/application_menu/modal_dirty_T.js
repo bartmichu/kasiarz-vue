b6713ed95cbaf3734df3d54c48a3b72009183e31
Michał Bartkowiak <bartmichu@gmail.com>
2018-01-01 14:20:59 +0100

    Implementacja SUI

--- imports/ui/components/application_menu/modal_dirty_T.js
+++ imports/ui/components/application_menu/modal_dirty_T.js
@@ -1,11 +1,9 @@
 import { Template } from "meteor/templating";
 import { $ } from "meteor/jquery";
+import "/public/semantic/semantic.min.js";
 import "./modal_dirty_T.html";
 
 
 Template.modal_dirty_T.rendered = () => {
-  $("#modal-dirty").modal({
-    dismissible: false,
-    opacity: 0.8,
-  });
+  $("#modal-dirty").modal("setting", "closable", false).modal("setting", "duration", 100);
 };
