60a7a50702e5abe84af133620e911de53249e444
Michał Bartkowiak <bartmichu@gmail.com>
2018-01-09 21:42:55 +0100

    Przerobienie templatki na SUI

--- imports/ui/pages/models_item_T.js
+++ imports/ui/pages/models_item_T.js
@@ -1,10 +1,9 @@
-import { Meteor } from "meteor/meteor";
 import { Session } from "meteor/session";
 import { Template } from "meteor/templating";
 import { Tracker } from "meteor/tracker";
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 import { $ } from "meteor/jquery";
-import { getAddingModeFromRoute, setEditMode, setFormLabels, setFormValues } from "/imports/util/client/client-functions.js";
+import { getAddingModeFromRoute, setEditMode, setFormLabels, setFormValues, setDirty } from "/imports/util/client/client-functions.js";
 import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
 import "/imports/ui/components/loading/loading_T.js";
 import "/imports/ui/components/item_menu/item_menu_cancel_T.js";
@@ -12,6 +11,7 @@ import "/imports/ui/components/item_menu/item_menu_close_T.js";
 import "/imports/ui/components/item_menu/item_menu_delete_T.js";
 import "/imports/ui/components/item_menu/item_menu_edit_T.js";
 import "/imports/ui/components/item_menu/item_menu_save_T.js";
+import "/public/semantic/semantic.min.js";
 import "./models_item_T.html";
 
 
@@ -21,13 +21,21 @@ Template.models_item_T.onCreated(() => {
 
 
 Template.models_item_T.rendered = () => {
-  $("select").material_select();
+  $("#dropdown-model-manufacturer").dropdown({
+    onChange() {
+      if (Session.equals("isEditMode", true)) {
+        setDirty(true);
+      }
+    },
+  });
+
   const template = Template.instance();
 
   if (getAddingModeFromRoute()) {
     template.subscribe("manufacturers.private", "", () => {
       Tracker.afterFlush(() => {
         setFormLabels();
+        $("#dropdown-model-manufacturer").dropdown();
       });
     });
   } else {
@@ -37,6 +45,7 @@ Template.models_item_T.rendered = () => {
         Tracker.afterFlush(() => {
           setFormLabels();
           setFormValues();
+          $("#dropdown-model-manufacturer").dropdown();
         });
       });
     });
@@ -47,12 +56,6 @@ Template.models_item_T.rendered = () => {
 Template.models_item_T.helpers({
   manufacturersH() {
     return Manufacturers.find({}, { sort: { nazwa: 1 } });
-  },
-  fixDisabledAttributeH() {
-    // HACK: odwrotny stan disabled bez defer
-    Meteor.defer(() => {
-      $("select").material_select();
-    });
   },
 });
 
