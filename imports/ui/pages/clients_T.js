a4926bfcb66f58afdceeddf3be87924f3e7a49a5
Michał Bartkowiak <bartmichu@gmail.com>
2018-01-14 15:10:03 +0100

    Dodanie tymczasowej templatki listy klientów

---
+++ imports/ui/pages/clients_T.js
@@ -0,0 +1,46 @@
+import { Template } from "meteor/templating";
+import { FlowRouter } from "meteor/ostrio:flow-router-extra";
+import { ReactiveVar } from "meteor/reactive-var";
+import Clients from "/imports/api/clients/clients.js";
+import { sortHandler } from "/imports/util/client/client-functions.js";
+import "/imports/ui/components/list_placeholder/list_placeholder_T.js";
+import "/imports/ui/components/loading/loading_T.js";
+import "/imports/ui/components/list_menu/list_menu_add_T.js";
+import "./clients_T.html";
+
+
+Template.clients_T.onCreated(() => {
+  Template.instance().subscribe("clients.private", "");
+  // TODO: ustawianie wartości domyślnej
+  Template.instance().sorfField = new ReactiveVar("dataModyfikacji");
+  Template.instance().sortOrder = new ReactiveVar("1");
+});
+
+
+Template.clients_T.helpers({
+  getSubscriptionDataH() {
+    const selector = {};
+    selector[Template.instance().sorfField.get()] = Template.instance().sortOrder.get();
+    return Clients.find({}, { sort: selector });
+  },
+  numberOfElementsH() {
+    return Clients.find().count();
+  },
+  isEmptyCollectionH() {
+    return Clients.find().count() === 0;
+  },
+  getLabelH(field) {
+    return Clients.simpleSchema().label(field);
+  },
+});
+
+
+Template.clients_T.events({
+  submit(event) {
+    event.preventDefault();
+  },
+  "click tbody tr": (event) => {
+    FlowRouter.go("clients.client", { _id: event.currentTarget.id });
+  },
+  "click thead th": (event, template) => sortHandler(event, template),
+});
