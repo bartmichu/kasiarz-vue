8007d5545adf84f6d03a850789975e399570b9cb
Michał Bartkowiak <bartmichu@gmail.com>
2018-01-14 15:09:49 +0100

    Dodanie listy klientów do głównego menu

--- imports/ui/components/application_menu/application_menu_T.js
+++ imports/ui/components/application_menu/application_menu_T.js
@@ -88,6 +88,13 @@ Template.application_menu_T.events({
       FlowRouter.go("employees");
     }
   },
+  "click #item-clients": () => {
+    if (Session.equals("isDirty", true)) {
+      $("#modal-dirty").modal("show");
+    } else {
+      FlowRouter.go("clients");
+    }
+  },
   "click #item-logout": () => {
     if (Session.equals("isDirty", true)) {
       $("#modal-dirty").modal("show");
