8c029ca8139b1c78bd597bdb1b8c5c5b6cc04a16
Michał Bartkowiak <bartmichu@gmail.com>
2018-01-15 13:58:37 +0100

    Zaimplementowanie metod serwisantów

---
+++ imports/api/employees/server/methods.js
@@ -0,0 +1,88 @@
+import { Meteor } from "meteor/meteor";
+import { ValidatedMethod } from "meteor/mdg:validated-method";
+import { check } from "meteor/check";
+import Shops from "/imports/api/shops/shops.js";
+import Employees from "/imports/api/employees/employees.js";
+
+
+export const insertEmployee = new ValidatedMethod({
+  name: "employees.insert",
+  validate(employee) {
+    const actualUserId = Meteor.userId();
+    if (actualUserId === null) {
+      throw new Meteor.Error("Błąd wywołania metody");
+    } else {
+      const shopId = employee.serwisId;
+      check(shopId, String);
+      const shop = Shops.findOne({ _id: shopId });
+      if (!shop || (shop.uzytkownikId !== actualUserId)) {
+        throw new Meteor.Error("Błąd wywołania metody");
+      }
+
+      employee.uzytkownikId = actualUserId;
+
+      const validationContext = Employees.simpleSchema().newContext();
+      if (validationContext.validate(employee) !== true) {
+        throw new Meteor.Error("Błąd wywołania metody");
+      }
+    }
+  },
+  run(employee) {
+    employee.uzytkownikId = Meteor.userId();
+    employee.dataUtworzenia = new Date();
+    employee.dataModyfikacji = employee.dataUtworzenia;
+    return Employees.insert(employee);
+  },
+});
+
+
+export const removeEmployee = new ValidatedMethod({
+  name: "employees.remove",
+  validate(documentId) {
+    const actualUserId = Meteor.userId();
+    if (actualUserId === null) {
+      throw new Meteor.Error("Błąd wywołania metody");
+    } else {
+      check(documentId, String);
+      const employee = Employees.findOne({ _id: documentId });
+      if (!employee || (employee.uzytkownikId !== actualUserId)) {
+        throw new Meteor.Error("Błąd wywołania metody");
+      }
+    }
+  },
+  run(documentId) {
+    return Employees.remove({ _id: documentId });
+  },
+});
+
+
+export const updateEmployee = new ValidatedMethod({
+  name: "employees.update",
+  validate({ documentId, formData }) {
+    const actualUserId = Meteor.userId();
+    if (actualUserId === null) {
+      throw new Meteor.Error("Błąd wywołania metody");
+    } else {
+      check(documentId, String);
+      const validationContext = Employees.simpleSchema().newContext();
+      if (validationContext.validate(formData) === true) {
+        const employee = Employees.findOne({ _id: documentId });
+        if (!employee || (employee.uzytkownikId !== actualUserId)) {
+          throw new Meteor.Error("Błąd wywołania metody");
+        }
+      } else {
+        throw new Meteor.Error("Błąd wywołania metody");
+      }
+    }
+  },
+  run({ documentId, formData }) {
+    return Employees.update({ _id: documentId }, {
+      $set: {
+        imieNazwisko: formData.imieNazwisko,
+        serwisId: formData.serwisId,
+        dodatkoweInformacje: formData.dodatkoweInformacje,
+        dataModyfikacji: new Date(),
+      },
+    });
+  },
+});
