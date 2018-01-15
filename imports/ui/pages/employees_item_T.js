629faac6ad0368daa6b23aef474b5ab3f540b093
Michał Bartkowiak <bartmichu@gmail.com>
2018-01-15 13:48:21 +0100

    Dodanie wczesnej wersji templatki danych serwisanta

---
+++ imports/ui/pages/employees_item_T.js
@@ -0,0 +1,78 @@
+import { Session } from "meteor/session";
+import { Template } from "meteor/templating";
+import { Tracker } from "meteor/tracker";
+import { FlowRouter } from "meteor/ostrio:flow-router-extra";
+import { $ } from "meteor/jquery";
+import { getAddingModeFromRoute, setEditMode, setFormLabels, setFormValues, setDirty, jqEscapeAndHash } from "/imports/util/client/client-functions.js";
+import Shops from "/imports/api/shops/shops.js";
+import "/imports/ui/components/loading/loading_T.js";
+import "/imports/ui/components/item_menu/item_menu_cancel_T.js";
+import "/imports/ui/components/item_menu/item_menu_close_T.js";
+import "/imports/ui/components/item_menu/item_menu_delete_T.js";
+import "/imports/ui/components/item_menu/item_menu_edit_T.js";
+import "/imports/ui/components/item_menu/item_menu_save_T.js";
+import "/public/semantic/semantic.min.js";
+import "./employees_item_T.html";
+
+
+Template.employees_item_T.onCreated(() => {
+  setEditMode(getAddingModeFromRoute());
+});
+
+
+Template.employees_item_T.rendered = () => {
+  const template = Template.instance();
+  if (getAddingModeFromRoute()) {
+    template.subscribe("shops.private", "", () => {
+      Tracker.afterFlush(() => {
+        setFormLabels();
+        $(jqEscapeAndHash("dropdown-serwisId")).dropdown({
+          onChange() {
+            if (Session.equals("isEditMode", true)) {
+              setDirty(true);
+            }
+          },
+        });
+      });
+    });
+  } else {
+    const employeeId = FlowRouter.getParam("_id");
+    template.subscribe("employees.private", employeeId, "", () => {
+      template.subscribe("shops.private", "", () => {
+        Tracker.afterFlush(() => {
+          setFormLabels();
+          setFormValues();
+          $(jqEscapeAndHash("dropdown-shopId")).dropdown({
+            onChange() {
+              if (Session.equals("isEditMode", true)) {
+                setDirty(true);
+              }
+            },
+          });
+        });
+      });
+    });
+  }
+};
+
+
+Template.employees_item_T.helpers({
+  shopsH() {
+    return Shops.find({}, { sort: { imieNazwisko: 1 } });
+  },
+});
+
+
+Template.employees_item_T.events({
+  submit(event) {
+    event.preventDefault();
+  },
+  "input input, input textarea": () => {
+    if (Session.equals("isEditMode", true)) {
+      setDirty(true);
+    }
+  },
+  "blur input, blur textarea": (event) => {
+    event.target.value = event.target.value.trim();
+  },
+});
