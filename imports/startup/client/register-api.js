df6eb4e133b0099774ed8b9e7aad36ed6eb09de7
Michał Bartkowiak <bartmichu@gmail.com>
2018-02-10 13:02:40 +0100

    Poprawienie czytelności kodu

--- imports/startup/client/register-api.js
+++ imports/startup/client/register-api.js
@@ -1,12 +1,17 @@
 import "/imports/api/manufacturers/manufacturers.js";
 import "/imports/api/manufacturers/schema.js";
+
 import "/imports/api/models/models.js";
 import "/imports/api/models/schema.js";
+
 import "/imports/api/shops/shops.js";
 import "/imports/api/shops/schema.js";
+
 import "/imports/api/offices/offices.js";
 import "/imports/api/offices/schema.js";
+
 import "/imports/api/employees/employees.js";
 import "/imports/api/employees/schema.js";
+
 import "/imports/api/clients/clients.js";
 import "/imports/api/clients/schema.js";
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -2,22 +2,27 @@ import "/imports/api/manufacturers/manufacturers.js";
 import "/imports/api/manufacturers/schema.js";
 import "/imports/api/manufacturers/server/publications.js";
 import "/imports/api/manufacturers/server/methods.js";
+
 import "/imports/api/models/models.js";
 import "/imports/api/models/schema.js";
 import "/imports/api/models/server/publications.js";
 import "/imports/api/models/server/methods.js";
+
 import "/imports/api/shops/shops.js";
 import "/imports/api/shops/schema.js";
 import "/imports/api/shops/server/publications.js";
 import "/imports/api/shops/server/methods.js";
+
 import "/imports/api/offices/offices.js";
 import "/imports/api/offices/schema.js";
 import "/imports/api/offices/server/publications.js";
 import "/imports/api/offices/server/methods.js";
+
 import "/imports/api/employees/employees.js";
 import "/imports/api/employees/schema.js";
 import "/imports/api/employees/server/publications.js";
 import "/imports/api/employees/server/methods.js";
+
 import "/imports/api/clients/clients.js";
 import "/imports/api/clients/schema.js";
 import "/imports/api/clients/server/publications.js";
