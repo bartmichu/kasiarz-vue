f5c6b994aab3c69a2ab9e6d307f43174b4ddba40
Michał Bartkowiak <bartmichu@gmail.com>
2018-02-17 19:17:46 +0100

    Dodanie pliku z uniwersalnymi funkcjami serwera

---
+++ imports/util/server/server-functions.js
@@ -0,0 +1,12 @@
+import { Match, check } from "meteor/check";
+
+
+const isNonEmptyString = Match.Where((x) => {
+  check(x, String);
+  return x.length > 0;
+});
+
+
+export {
+  isNonEmptyString,
+}
