c6d1cd2c1864cf37368c3311b37884b1748cfa36
Michał Bartkowiak <bartmichu@gmail.com>
2018-02-24 13:17:36 +0100

    Dodanie mapy UI

---
+++ imports/util/client/uimap.js
@@ -0,0 +1,94 @@
+const uimap = {
+  models: {
+    header: "Modele urządzeń",
+    crumbs: ["Konfiguracja", "Modele"],
+    buttons: {
+      close: true,
+    },
+  },
+  model: {
+    header: "Dane modelu",
+    crumbs: ["Konfiguracja", "Modele", "Model"],
+    buttons: {
+      delete: true,
+      close: true,
+    },
+  },
+  manufacturers: {
+    header: "Producenci urządzeń",
+    crumbs: ["Konfiguracja", "Producenci"],
+    buttons: {
+      close: true,
+    },
+  },
+  manufacturer: {
+    header: "Dane producenta",
+    crumbs: ["Konfiguracja", "Producenci", "Producent"],
+    buttons: {
+      delete: true,
+      close: true,
+    },
+  },
+  shops: {
+    header: "Serwisy",
+    crumbs: ["Konfiguracja", "Serwisy"],
+    buttons: {
+      close: true,
+    },
+  },
+  shop: {
+    header: "Dane serwisu",
+    crumbs: ["Konfiguracja", "Serwisy", "Serwis"],
+    buttons: {
+      close: true,
+    },
+  },
+  employees: {
+    header: "Serwisanci",
+    crumbs: ["Konfiguracja", "Serwisanci"],
+    buttons: {
+      close: true,
+    },
+  },
+  employee: {
+    header: "Dane serwisanta",
+    crumbs: ["Konfiguracja", "Serwisanci", "Serwisant"],
+    buttons: {
+      delete: true,
+      close: true,
+    },
+  },
+  offices: {
+    header: "Urzędy skarbowe",
+    crumbs: ["Konfiguracja", "Urzędy skarbowe"],
+    buttons: {
+      close: true,
+    },
+  },
+  office: {
+    header: "Dane urzędu",
+    crumbs: ["Konfiguracja", "Urzędy skarbowe", "Urząd"],
+    buttons: {
+      delete: true,
+      close: true,
+    },
+  },
+  clients: {
+    header: "Klienci",
+    crumbs: ["Klienci"],
+    buttons: {
+      close: true,
+    },
+  },
+  client: {
+    header: "Dane klienta",
+    crumbs: ["Klienci", "Klient"],
+    buttons: {
+      delete: true,
+      close: true,
+    },
+  },
+};
+
+
+export default uimap;
