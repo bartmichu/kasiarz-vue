0d03afd94b9161c2d01a12b771215501cc60e524
Michał Bartkowiak <bartmichu@gmail.com>
2018-02-25 14:54:14 +0100

    Dodanie API listy województw

---
+++ imports/api/voivodeships/server/publications.js
@@ -0,0 +1,13 @@
+import { Meteor } from "meteor/meteor";
+import Voivodeships from "/imports/api/voivodeships/voivodeships.js";
+
+
+Meteor.publish("voivodeships.public", function publishFunction() {
+  const data = Voivodeships.find({});
+
+  if (data) {
+    return data;
+  }
+
+  return this.ready();
+});
