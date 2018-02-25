eb286bee74fb1b65733b40d66e3da8019f6f8a9a
Michał Bartkowiak <bartmichu@gmail.com>
2018-02-25 14:54:58 +0100

    Dodanie automatycznego uzupełniania bazy województw

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,6 +2,7 @@ import "/imports/startup/server/browserpolicy-configuration.js";
 import "/imports/startup/server/accounts-configuration.js";
 import "/imports/startup/server/accounts-fixtures.js";
 import "/imports/startup/server/register-api.js";
+import "/imports/startup/server/voivodeships-fixtures.js";
 
 // TODO: usunąć w wersji produkcyjnej
 import "/imports/startup/server/manufacturers-fixtures.js";
