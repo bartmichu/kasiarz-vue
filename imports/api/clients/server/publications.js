b7200e8f5bc95518b62582b8862ef2187e36c02f
Michał Bartkowiak <bartmichu@gmail.com>
2018-03-04 22:36:37 +0100

    Dodanie NIP na liście klientów

--- imports/api/clients/server/publications.js
+++ imports/api/clients/server/publications.js
@@ -7,7 +7,7 @@ import { isNonEmptyString } from "/imports/util/server/server-functions.js";
 Meteor.publish("clients.list", function publishFunction() {
   const actualUserId = Meteor.userId();
   if (actualUserId) {
-    const data = Clients.find({ uzytkownikId: actualUserId }, { fields: { nazwa: 1, "adres.miejscowosc": 1, dataModyfikacji: 1 } });
+    const data = Clients.find({ uzytkownikId: actualUserId }, { fields: { nazwa: 1, nip: 1, "adres.miejscowosc": 1, dataModyfikacji: 1 } });
 
     if (data) {
       return data;
