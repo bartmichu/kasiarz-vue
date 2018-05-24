aceab84d5b8ef52497230d6077a711b3fb135e4d
Michał Bartkowiak <bartmichu@gmail.com>
2018-05-24 19:54:58 +0200

    Dodanie komunikatu do wskaźnika pobierania danych

--- imports/ui/components/LoadingIndicator.vue
+++ imports/ui/components/LoadingIndicator.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-progress-linear indeterminate color="grey" height="20"></v-progress-linear>
+  <div>
+    <span class="">Trwa pobieranie danych z serwera...</span>
+    <v-progress-linear indeterminate color="grey" height="20"></v-progress-linear>
+  </div>
 </template>
 
 
