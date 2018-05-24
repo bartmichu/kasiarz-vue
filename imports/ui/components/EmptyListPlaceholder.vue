7e741018abf3b5d78a853653daba9a5b8dbf93c6
Michał Bartkowiak <bartmichu@gmail.com>
2018-05-24 06:15:14 +0200

    Dodanie komponentu zamiennika pustej listy

---
+++ imports/ui/components/EmptyListPlaceholder.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-alert value="true" color="success" icon="info">
+    Lista jest pusta. Użyj przycisku "dodaj" aby utworzyć nowy element.
+  </v-alert>
+</template>
+
+
+<script>
+export default {
+  name: "EmptyListPlaceholder"
+};
+</script>
