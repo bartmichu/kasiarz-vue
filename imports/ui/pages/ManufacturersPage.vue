5030677fcf5c15e5f732ca9f8f1834a56964bd1c
Michał Bartkowiak <bartmichu@gmail.com>
2018-05-25 06:55:44 +0200

    Dodanie wczesnej wersji listy producentów z subskrypcją

--- imports/ui/pages/ManufacturersPage.vue
+++ imports/ui/pages/ManufacturersPage.vue
@@ -1,10 +1,82 @@
 <template>
-  <p>manufacturers</p>
+  <v-card>
+
+    <v-toolbar card color="grey lighten-2">
+      <v-toolbar-title>Lista producentów urządzeń</v-toolbar-title>
+    </v-toolbar>
+
+    <v-card-text v-if="!isSubscriptionReady">
+      <LoadingIndicator></LoadingIndicator>
+    </v-card-text>
+
+    <v-card-text v-else>
+      <EmptyListPlaceholder v-if="isEmptyCollection" message="Lista producentów jest pusta."></EmptyListPlaceholder>
+
+      <v-data-table v-else :headers="tableHeaders" :items="subscribedData" hide-actions>
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.nazwa }}</td>
+          <td class="text-xs-right">{{ props.item.adres.miejscowosc }}</td>
+          <td class="text-xs-right">{{ props.item.dataModyfikacji }}</td>
+        </template>
+      </v-data-table>
+    </v-card-text>
+
+  </v-card>
 </template>
 
 
 <script>
+import EmptyListPlaceholder from "/imports/ui/components/EmptyListPlaceholder.vue";
+import LoadingIndicator from "/imports/ui/components/LoadingIndicator.vue";
+import Manufacturers from "/imports/api/manufacturers/manufacturers.js";
+
 export default {
-  name: "ManufacturersPage"
+  name: "ManufacturersPage",
+
+  components: {
+    EmptyListPlaceholder,
+    LoadingIndicator
+  },
+
+  meteor: {
+    $subscribe: {
+      "manufacturers.list": []
+    },
+    subscribedData() {
+      return Manufacturers.find({});
+    }
+  },
+
+  data() {
+    return {
+      tableHeaders: [
+        {
+          text: "Pełna nazwa",
+          align: "left",
+          value: "nazwa"
+        },
+        {
+          text: "Miejscowość",
+          align: "right",
+          value: "adres.miejscowosc"
+        },
+        {
+          text: "Data modyfikacji",
+          align: "right",
+          value: "dataModyfikacji"
+        }
+      ],
+      items: []
+    };
+  },
+
+  computed: {
+    isSubscriptionReady() {
+      return this.$subReady["manufacturers.list"];
+    },
+    isEmptyCollection() {
+      return this.subscribedData.length === 0;
+    }
+  }
 };
 </script>
