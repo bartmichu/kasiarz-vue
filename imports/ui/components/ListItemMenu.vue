d3311f1cc7b9e8aa55e415b86d22b80b3b61c849
Michał Bartkowiak <bartmichu@gmail.com>
2018-05-29 20:26:59 +0200

    Tymczasowe przeniesienie okna potwierdzającego usunięcie pozycji

--- imports/ui/components/ListItemMenu.vue
+++ imports/ui/components/ListItemMenu.vue
@@ -10,18 +10,27 @@
       <v-list-tile @click="">
         <v-list-tile-title>edytuj</v-list-tile-title>
       </v-list-tile>
-      <v-list-tile @click.native.stop="deleteModal = true">
+      <v-list-tile @click.stop="isDeleteModalVisible = true">
         <v-list-tile-title>usuń</v-list-tile-title>
-        <DeleteItemModal></DeleteItemModal>
       </v-list-tile>
     </v-list>
+
+    <v-dialog v-model="isDeleteModalVisible" persistent max-width="60%">
+      <v-card>
+        <v-card-title class="headline">Usuń element</v-card-title>
+        <v-card-text>Usuwanie jest operacją nieodwracalną - czy chcesz trwale usunąć wybrany element?</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" flat @click.native="isDeleteModalVisible = false">Nie usuwaj</v-btn>
+          <v-btn color="green darken-1" flat @click.native="isDeleteModalVisible = false">Usuń</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-menu>
 </template>
 
 
 <script>
-import DeleteItemModal from "/imports/ui/components/DeleteItemModal.vue";
-
 export default {
   name: "ListItemMenu",
 
@@ -35,12 +44,10 @@ export default {
 
   data() {
     return {
-      deleteModal: false
+      isDeleteModalVisible: false
     };
   },
 
-  components: {
-    DeleteItemModal
-  }
+  components: {}
 };
 </script>
