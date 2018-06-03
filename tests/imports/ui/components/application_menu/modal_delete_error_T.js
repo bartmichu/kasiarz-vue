4bc798d70f65be9bc65a7aabb0f2e4a643151701
Michał Bartkowiak <bartmichu@gmail.com>
2018-06-03 12:17:08 +0200

    Usunięcie zbędnych plików

--- tests/imports/ui/components/application_menu/modal_delete_error_T.js
+++
@@ -1,9 +0,0 @@
-import { Template } from "meteor/templating";
-import { $ } from "meteor/jquery";
-import "/public/semantic/semantic.min.js";
-import "./modal_delete_error_T.html";
-
-
-Template.modal_delete_error_T.rendered = () => {
-  $("#modal-delete-error").modal("setting", "closable", false).modal("setting", "duration", 100);
-};
