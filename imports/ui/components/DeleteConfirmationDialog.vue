a762b9f7055f111878296ad3572aef32f32ca7e5
Michał Bartkowiak <bartmichu@gmail.com>
2018-06-03 12:17:29 +0200

    Implementacja usuwania elementu listy

--- imports/ui/components/DeleteConfirmationDialog.vue
+++ imports/ui/components/DeleteConfirmationDialog.vue
@@ -1,18 +1,33 @@
 <template>
+
   <v-dialog v-model="isOpened" :fullscreen="$vuetify.breakpoint.xs" persistent max-width="50%">
-    <v-card>
+
+    <v-card :color="getCardColor">
+
       <v-card-title class="headline">{{ title }}</v-card-title>
-      <v-card-text>Usuwanie jest operacją nieodwracalną.</v-card-text>
-      <v-card-actions>
+
+      <v-card-text v-if="actionFailed">Usunięcie danego elementu nie jest w tej chwili możliwe, ponieważ odnoszą się do niego inne elementy bazy danych.</v-card-text>
+      <v-card-text v-else>Usuwanie jest operacją nieodwracalną.</v-card-text>
+
+      <v-card-actions v-if="actionFailed">
+        <v-spacer></v-spacer>
+        <v-btn color="secondary" @click.native="closeDialog">OK</v-btn>
+      </v-card-actions>
+      <v-card-actions v-else>
         <v-spacer></v-spacer>
         <v-btn color="secondary" @click.native="closeDialog">Anuluj</v-btn>
         <v-btn color="error" @click.native="deleteItem">Tak, usuń</v-btn>
       </v-card-actions>
+
     </v-card>
+
   </v-dialog>
+
 </template>
 
 <script>
+import { Meteor } from "meteor/meteor";
+
 export default {
   name: "DeleteConfirmationDialog",
 
@@ -24,9 +39,26 @@ export default {
     }
   },
 
+  data() {
+    return {
+      actionFailed: false
+    };
+  },
+
   computed: {
     isOpened() {
       return this.$store.state.deleteConfirmationDialog;
+    },
+    getCardColor() {
+      return this.actionFailed ? "error" : "";
+    }
+  },
+
+  watch: {
+    isOpened() {
+      if (this.$store.state.deleteConfirmationDialog) {
+        this.actionFailed = false;
+      }
     }
   },
 
@@ -36,8 +68,17 @@ export default {
       this.$store.commit("closeDeleteConfirmationDialog");
     },
     deleteItem() {
-      this.$store.commit("resetSelectedListItemId");
-      this.$store.commit("closeDeleteConfirmationDialog");
+      Meteor.call(
+        this.$router.currentRoute.name.concat(".remove"),
+        this.$store.state.selectedListItemId,
+        error => {
+          if (error) {
+            this.actionFailed = true;
+          } else {
+            this.closeDialog();
+          }
+        }
+      );
     }
   }
 };
