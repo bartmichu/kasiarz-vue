6f4d1ee55f806394c2f56adb33590f61b693d84d
Michał Bartkowiak <bartmichu@gmail.com>
2018-06-16 15:32:47 +0200

    Dodanie zamiennika pustej listy powiązanych elementów

---
+++ imports/ui/components/RelatedItemsPlaceholder.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <v-alert value="true" color="grey" icon="info">
+    Lista powiązanych elementów jest pusta.
+  </v-alert>
+
+</template>
+
+
+<script>
+export default {
+  name: "EmptyListPlaceholder"
+};
+</script>
