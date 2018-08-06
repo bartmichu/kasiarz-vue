21992abbd125e0ed0388c647f0f9ec0cc967a14c
Michał Bartkowiak <bartmichu@gmail.com>
2018-08-06 15:10:25 +0200

    Dodanie kolekcji po stronie klienta

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,4 +1,5 @@
 import "/imports/startup/client/accounts-configuration.js";
+import "/imports/startup/client/register-api.js";
 import "/imports/startup/client/vue.js";
 import "/imports/startup/client/router.js";
 // import "/imports/startup/client/store.js";
---
+++ imports/startup/client/register-api.js
@@ -0,0 +1,23 @@
+import "/imports/api/manufacturers/manufacturers.js";
+import "/imports/api/manufacturers/schema.js";
+
+import "/imports/api/models/models.js";
+import "/imports/api/models/schema.js";
+
+import "/imports/api/shops/shops.js";
+import "/imports/api/shops/schema.js";
+
+import "/imports/api/offices/offices.js";
+import "/imports/api/offices/schema.js";
+
+import "/imports/api/employees/employees.js";
+import "/imports/api/employees/schema.js";
+
+import "/imports/api/clients/clients.js";
+import "/imports/api/clients/schema.js";
+
+import "/imports/api/devices/devices.js";
+import "/imports/api/devices/schema.js";
+
+import "/imports/api/voivodeships/voivodeships.js";
+import "/imports/api/voivodeships/schema.js";
