b5483230061495c7038b740591f7df6be660ecda
Michał Bartkowiak <bartmichu@gmail.com>
2018-10-05 19:26:57 +0200

    Zmiana formatowania kodu

--- imports/ui/components/LoadingIndicator.vue
+++ imports/ui/components/LoadingIndicator.vue
@@ -2,7 +2,7 @@
 
   <div>
     <span>Trwa pobieranie danych z serwera...</span>
-    <v-progress-linear indeterminate color="grey" height="20"/>
+    <v-progress-linear indeterminate color="grey" height="20" />
   </div>
 
 </template>
